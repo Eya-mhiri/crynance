4d0aef53163ecd3f2fcb4dc000c24f79dfae081e
Eya-mhiri <aya.mhiri@ensi-uma.tn>
2025-05-07 19:31:12 +0200

    Create user.php

---
+++ user.php
@@ -0,0 +1,44 @@
+<?php
+class User {
+  public $id;
+  public $email;
+  public $password_hash;
+  public $created_at;
+
+  public static function find($id) {
+    global $pdo;
+    
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+    $stmt->execute([$id]);
+    return $stmt->fetchObject(__CLASS__);
+  }
+
+  public static function authenticate($email, $password) {
+    global $pdo;
+    
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetchObject(__CLASS__);
+    
+    if (!$user || !password_verify($password, $user->password_hash)) {
+      throw new Exception("Identifiants invalides");
+    }
+    
+    return $user;
+  }
+
+  public static function register($email, $password) {
+    global $pdo;
+    
+    // Validation supplémentaire...
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+    
+    $stmt = $pdo->prepare("
+      INSERT INTO users (email, password_hash) 
+      VALUES (?, ?)
+    ");
+    $stmt->execute([$email, $password_hash]);
+    
+    return self::find($pdo->lastInsertId());
+  }
+}
