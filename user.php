61a965bf75a7aac77c439b60e6cd17ddd85cd862
Eya-mhiri <aya.mhiri@ensi-uma.tn>
2025-05-07 21:38:32 +0200

    Update user.php

--- user.php
+++ user.php
@@ -1,4 +1,9 @@
 <?php
+// En HAUT du fichier
+require_once __DIR__ . '/../includes/config.php'; // Configuration DB
+require_once __DIR__ . '/../includes/header.php'; // Header HTML
+?>
+<?php
 class User {
   public $id;
   public $email;
