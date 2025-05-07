7287e7d8cdce3578cdb4cfa02f0e266e7125f284
Eya-mhiri <aya.mhiri@ensi-uma.tn>
2025-05-07 21:32:51 +0200

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,1 +1,10 @@
+<?php
+require_once __DIR__ . '/../includes/config.php';
 
+// Destruction de la session
+$_SESSION = [];
+session_destroy();
+
+// Redirection vers la page de login
+redirect('auth/login.php');
+?>
